1040dd8d845597aa45044ef11154e030914a1a1a
Dev-Atharv <invasionyt19@gmail.com>
2025-05-27 21:31:18 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,17 +1,15 @@
-import flask
-from flask import Flask, request, jsonify, render_template
-import os
-import sys
+from flask import Flask, render_template, jsonify 
 
 app = Flask(__name__)
-# Configure logging
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
     data = {"message": "This is some sample data"}
     return jsonify(data)
 
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
